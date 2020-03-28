1dcbe9446a483435a99b8d0dfbef64a58e3d1bd3
Michael Kwok <michael.kwok00@gmail.com>
2020-03-27 18:28:33 -0600

    Added comments

--- client/serial_handling.cpp
+++ client/serial_handling.cpp
@@ -2,31 +2,63 @@
 
 extern shared_vars shared;
 
-// TODO: Documentation
-
+/**
+ * Function to get pointer to beginning of string, for use like an iterator
+ * 
+ * Inspired by my use of this function while writing the assignment using
+ * an updated version of the Arduino SDK
+ * 
+ * @param str: String object to get "iterator" for.
+ * @return: "Iterator" for use
+ */
 char* begin(const String& str) {
+    // Force cast from const char* to char* 
   return (char*)str.c_str();
 }
 
+/**
+ * Function to get pointer to end of string, for use like an iterator
+ * 
+ * Inspired by my use of this function while writing the assignment using
+ * an updated version of the Arduino SDK, similar to the above.
+ * 
+ * @param str: String object to get "iterator" for.
+ * @return: "Iterator" for use
+ */
 char* end(const String& str) {
+  // Force cast from const char* to char* 
   return (char*)(str.c_str() + str.length());
 }
 
-
-void process_waypoint(String& line, lon_lat_32* waypoint_array) {
-  // Find space
+/**
+ * Process Waypoint message and stores it into the waypoint array
+ * 
+ * @param line: Waypoint message to process.
+ * @param waypoint_array: Pointer to array entry where the data should be stored.
+ */
+void process_waypoint(String& line, lon_lat_32* const waypoint_array) {
   for (auto it = begin(line); it != end(line); it++) {
     if (*it == ' ') {
-      // We found space
+      // Space means time to split
       int32_t latitude = line.substring(0, it -begin(line)).toInt();
       int32_t longitude =
           line.substring(it - begin(line) + 1, line.length() - 1).toInt();
       *waypoint_array = lon_lat_32(longitude, latitude);
+      // Stop function early
       return;
     }
   }
 }
 
+/**
+ * Read a line from serial, with timeout set to 1000ms by default
+ * 
+ * Text is continuously read from serial until \n or \r is hit.
+ * 
+ * @param line: Reference to where the read line should be store
+ * @param timeout: How long the timeout should be in milliseconds
+ * @return: true if success, false if timed out.
+ */
 bool read_line(String& line, unsigned long timeout = 1000) {
   char buffer[22];
 
@@ -35,8 +67,7 @@ bool read_line(String& line, unsigned long timeout = 1000) {
 
   char input = ' ';
 
-  // while ((millis() - start_time) <= timeout) {
-    while (true) {
+  while ((millis() - start_time) <= timeout) {
     if (Serial.available()) {
       input = Serial.read();
       buffer[i++] = input;
@@ -50,6 +81,15 @@ bool read_line(String& line, unsigned long timeout = 1000) {
   return false;
 }
 
+/**
+ * Communicates with server over serial
+ * 
+ * This function runs until last waypoint has been sent.
+ * 
+ * @param start: Start waypoint
+ * @param end: End waypoint
+ * @return: 0 if failed for any reason, 1 if not.
+ */
 uint8_t get_waypoints(const lon_lat_32& start, const lon_lat_32& end) {
   String line;
 
@@ -63,7 +103,9 @@ uint8_t get_waypoints(const lon_lat_32& start, const lon_lat_32& end) {
   Serial.print(" ");
   Serial.println(end.lon);
 
+  // Wait for number of waypoints
   if (read_line(line, 10000) && line[0] == 'N') {
+    // Did not timeout, send ACK
     line = line.substring(2);
     shared.num_waypoints = line.toInt();
     Serial.println("A");
@@ -71,18 +113,18 @@ uint8_t get_waypoints(const lon_lat_32& start, const lon_lat_32& end) {
     return 0;
   }
 
+  // Process following waypoints
   for (auto i = 0; i < shared.num_waypoints; i++) {
     if (read_line(line)  && line[0] == 'W') {
+      // Process new input
       line = line.substring(2);
       process_waypoint(line, shared.waypoints + i);
       Serial.println("A");
     } else {
+      // Timed out
       return 0;
     }
   }
-
-  // 1 indicates a successful exchange, of course you should only output 1
-  // in your final solution if the exchange was indeed successful
-  // (otherwise, return 0 if the communication failed)
+  // Skip waiting for E by server because num of waypoints defined beforehand.
   return 1;
 }
--- client/serial_handling.h
+++ client/serial_handling.h
@@ -5,23 +5,15 @@
 #include "map_drawing.h"
 #include <Arduino.h>
 
-/*
-   Your job!
-
-   Print the request with the given endpoints and read the replies
-   from the server. Store the number of waypoints and the waypoints themselves
-   in the appropriate fields of the extern variable shared
-
-   Returns 1 if successful, otherwise 0.
-
-   You can feel free to modify this as you see fit. If you want to break
-   the steps into multiple functions, then go ahead.
-
-   If the number of waypoints is > max_waypoints (from consts_and_types.h),
-   print some message using status_message() indicating the path is too long,
-   delay for 5 seconds, and then resume having treated this like there is no
-   path.
-*/
+/**
+ * Communicates with server over serial
+ * 
+ * This function runs until last waypoint has been sent.
+ * 
+ * @param start: Start waypoint
+ * @param end: End waypoint
+ * @return: 0 if failed for any reason, 1 if not.
+ */
 uint8_t get_waypoints(const lon_lat_32& start, const lon_lat_32& end);
 
 #endif
